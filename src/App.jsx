b0eeea8f48a3024a5451d58076ffafb950e6cb26
Rohan Singh <rs7289579@gmail.com>
2022-09-18 11:30:21 +0530

    fixing layout

--- src/App.jsx
+++ src/App.jsx
@@ -45,13 +45,13 @@ function App() {
   }
   return (
     <BrowserRouter>
-    <section className="flex flex-col bg-gray-900 h-full lg:p-10">
+    <section className="flex flex-col bg-gray-900 lg:p-10">
       <div className='flex flex-col lg:flex-row justify-evenly'>
         {/* ---------------------Left Side Bar -----------------------------*/}
         <div className='w-full h-14 lg:w-2/12 relative lg:h-[90vh]'>
           <div className='text-blue-500 text-2xl font-bold text-center flex items-center justify-between relative px-4'>
             <GiHamburgerMenu onClick={toggleSidebar} className="lg:hidden cursor-pointer mt-2"/> 
-          <div className="flex items-center my-1 lg:my-4 lg:ml-4 mt-2"><GiFuelTank className="ml-8 mr-2 -mt-1" /> <p>Fuel</p>
+          <div className="flex items-center my-1 lg:my-4 lg:ml-4 mt-2"><GiFuelTank className="ml-8 mr-2 -mt-1 text-3xl" /><p><span className="text-4xl text-blue-00"> F</span>uel</p>
           </div>
           <div onClick={()=>{setLogin(!login)}} className="w-7 h-7 bg-blue-500 rounded-full lg:hidden relative cursor-pointer mt-2">
           {login && <ProfileSection props={{setLogin , login}}/>}
@@ -59,23 +59,23 @@ function App() {
           </div>
           <div ref={ref} className='my-12 px-6 lg:px-0 py-14 lg:py-0 transform transition-transform -translate-x-full lg:translate-x-0 z-10 h-[100vh] absolute -top-10 bg-gray-900 left-0'>
             <ImCross onClick={toggleSidebar} className="absolute top-3 right-4 text-xl text-gray-500 lg:hidden cursor-pointer"/>
-            <ul className='flex flex-col items-center space-y-5'>
-              <Link to={"/"} className='w-40 text-gray-300 flex items-center cursor-pointer font-semibold -ml-10'><div className="flex items-center my-1 lg:my-4 lg:ml-4 text-3xl text-blue-500 -mt-4 lg:mt-0"><GiFuelTank className="ml-8 mr-2 -mt-1" /> <p>Fuel</p>
+            <ul className='flex flex-col items-center space-y-4 lg:mt-16'>
+              <Link to={"/"} className='w-40 text-gray-300 flex items-center cursor-pointer font-semibold -ml-10'><div className="flex items-center my-1 lg:my-4 lg:ml-4 text-3xl text-blue-500 -mt-4 lg:-mt-10"><GiFuelTank className="ml-8 mr-2 -mt-1 text-4xl" /> <p className="text-2xl"><span className="text-4xl text-blue-600"> F</span>uel</p>
           </div></Link>
-              <Link to={"/"} className={`w-40 text-gray-300 flex items-center cursor-pointer font-semibold hover:underline hover:underline-offset-4 hover:text-blue-500`}><MdOutlineSpaceDashboard className="text-xl mx-2" /> Dashboard</Link>
-              <Link to={"fuelefficiency"} className='w-40 text-gray-300 flex items-center cursor-pointer font-semibold hover:underline hover:underline-offset-4 hover:text-blue-500'><GiFuelTank className="text-xl mx-2" /> Fuel Efficiency</Link>
-              <Link to={"documents"} className='w-40 text-gray-300 flex items-center cursor-pointer font-semibold hover:underline hover:underline-offset-4 hover:text-blue-500'><TiDocumentText className="text-xl mx-2" /> Documents</Link>
-              <Link to={"messages"} className='w-40 text-gray-300 flex items-center cursor-pointer font-semibold hover:underline hover:underline-offset-4 hover:text-blue-500'><AiOutlineMessage className="text-xl mx-2" /> Messages</Link>
-              <Link to={"settings"} className='w-40 text-gray-300 flex items-center cursor-pointer font-semibold hover:underline hover:underline-offset-4 hover:text-blue-500'><FiSettings className="text-xl mx-2" /> Settings</Link>
-              <Link to={"calender"} className='w-40 text-gray-300 flex items-center cursor-pointer font-semibold hover:underline hover:underline-offset-4 hover:text-blue-500'><BsCalendar4Week className="text-xl mx-2" /> Calender</Link>
-              <Link to={"helpcenter"} className='w-40 text-gray-300 flex items-center cursor-pointer font-semibold hover:underline hover:underline-offset-4 hover:text-blue-500'><MdOutlineCall className="text-xl mx-2" /> Help Center</Link>
-              <Link to={"logout"} className='w-40 text-blue-600 flex items-center cursor-pointer font-semibold'><MdLogout className="text-xl mx-2 mt-28" /><span className="mt-28"> Log out</span></Link>
+              <Link to={"/"} className='w-40 text-gray-300 flex items-center cursor-pointer font-semibold hover:underline hover:underline-offset-4 hover:text-blue-500 hover:-translate-y-1  hover:translate-x-1 hover:duration-300 p-1'><MdOutlineSpaceDashboard className="text-2xl mx-2" /> Dashboard</Link>
+              <Link to={"fuelefficiency"} className='w-40 text-gray-300 flex items-center cursor-pointer font-semibold hover:underline hover:underline-offset-4 hover:text-blue-500 hover:-translate-y-1  hover:translate-x-1 hover:duration-300 p-1'><GiFuelTank className="text-2xl mx-2" /> Fuel Efficiency</Link>
+              <Link to={"documents"} className='w-40 text-gray-300 flex items-center cursor-pointer font-semibold hover:underline hover:underline-offset-4 hover:text-blue-500 hover:-translate-y-1  hover:translate-x-1 hover:duration-300 p-1'><TiDocumentText className="text-2xl mx-2" /> Documents</Link>
+              <Link to={"messages"} className='w-40 text-gray-300 flex items-center cursor-pointer font-semibold hover:underline hover:underline-offset-4 hover:text-blue-500 hover:-translate-y-1  hover:translate-x-1 hover:duration-300 p-1'><AiOutlineMessage className="text-2xl mx-2" /> Messages</Link>
+              <Link to={"settings"} className='w-40 text-gray-300 flex items-center cursor-pointer font-semibold hover:underline hover:underline-offset-4 hover:text-blue-500 hover:-translate-y-1  hover:translate-x-1 hover:duration-300 p-1'><FiSettings className="text-2xl mx-2" /> Settings</Link>
+              <Link to={"calender"} className='w-40 text-gray-300 flex items-center cursor-pointer font-semibold hover:underline hover:underline-offset-4 hover:text-blue-500 hover:-translate-y-1  hover:translate-x-1 hover:duration-300 p-1'><BsCalendar4Week className="text-2xl mx-2" /> Calender</Link>
+              <Link to={"helpcenter"} className='w-40 text-gray-300 flex items-center cursor-pointer font-semibold hover:underline hover:underline-offset-4 hover:text-blue-500 hover:-translate-y-1  hover:translate-x-1 hover:duration-300 p-1'><MdOutlineCall className="text-2xl mx-2" /> Help Center</Link>
+              <Link to={"logout"} className='w-40 text-blue-600 flex items-center cursor-pointer font-semibold'><MdLogout className="text-2xl mx-2 mt-28" /><span className="mt-28"> Log out</span></Link>
             </ul>
           </div>
         </div>
         {/* ----------------------Middle Section-------------------------------- */}
-        <div className='w-full lg:w-10/12 relative bg-gray-700 rounded-md flex justify-evenly lg:p-4'>
-          <div className="w-full lg:w-9/12 pl-3 pr-5">
+        <div className='w-full lg:w-10/12 relative bg-gray-700 rounded-md flex justify-evenly'>
+          <div className="w-full lg:w-10/12">
             {/* search bar */}
             {/* <div className="flex bg-gray-800 rounded-lg my-2 shadow-lg">
               <AiOutlineSearch className="text-2xl text-gray-200 font-semibold mt-1 mx-1" />
--- src/Components/FuelEfficiency.jsx
+++ src/Components/FuelEfficiency.jsx
@@ -9,70 +9,70 @@ import { RiPinDistanceLine } from 'react-icons/ri';
 const FuelEfficiency = () => {
 
   return (
-    <div className='lg:my-10'>
+    <div className='lg:my-8'>
             {/* options section */}
             <div className="flex justify-evenly items-center py-4 mx-auto flex-wrap lg:flex-nowrap">
-              <div className="bg-gray-800 w-56 md:w-44 rounded-md shadow-2xl py-2 flex justify-between px-8 md:px-2 md:justify-evenly">
+              <div className="bg-gray-800 w-56 rounded-md shadow-2xl py-2 flex justify-between px-8 md:px-2 md:justify-evenly cursor-pointer hover:-translate-y-1 hover:duration-300 hover:shadow-2xl">
                 <div className="h-full p-1">
                   <div className="p-1 bg-yellow-500 rounded-md w-fit my-1">
-                    <GiFuelTank className="text-3xl text-gray-100" />
+                    <GiFuelTank className="text-5xl text-gray-100" />
                   </div>
                 </div>
                 <div className='flex flex-col justify-center'>
-                  <p className="text-gray-100">6,548 L</p>
+                  <p className="text-gray-100 text-lg">6,548 L</p>
                   <p className="text-gray-400">Fuel&nbsp;Usage</p>
                 </div>
               </div>
-              <div className="bg-gray-800 w-56 md:w-44 rounded-md shadow-2xl py-2 flex justify-between px-8 md:px-2 md:justify-evenly my-2 md:my-0">
+              <div className="bg-gray-800 w-56 rounded-md shadow-2xl py-2 flex justify-between px-8 md:px-2 md:justify-evenly my-2 md:my-0 cursor-pointer hover:-translate-y-1 hover:duration-200 hover:shadow-2xl">
                 <div className="h-full p-1  ">
                   <div className="p-1 bg-blue-500 rounded-md w-fit my-1">
-                    <RiPinDistanceLine className="text-3xl text-gray-100" />
+                    <RiPinDistanceLine className="text-5xl text-gray-100" />
                   </div>
                 </div>
                 <div className='flex flex-col justify-center'>
-                  <p className="text-gray-100">31,875</p>
+                  <p className="text-gray-100 text-lg">31,875</p>
                   <p className="text-gray-400">Distance</p>
                 </div>
               </div>
-              <div className="bg-gray-800 w-56 md:w-44 rounded-md shadow-2xl py-2 flex justify-between px-8 md:px-2 md:justify-evenly">
+              <div className="bg-gray-800 w-56 rounded-md shadow-2xl py-2 flex justify-between px-8 md:px-2 md:justify-evenly cursor-pointer hover:-translate-y-1 hover:duration-200 hover:shadow-2xl">
                 <div className="h-full p-1  ">
                   <div className="p-1 bg-cyan-500 rounded-md w-fit my-1">
-                    <AiOutlineFieldTime className="text-3xl text-gray-100" />
+                    <AiOutlineFieldTime className="text-5xl text-gray-100" />
                   </div>
                 </div>
                 <div className='flex flex-col justify-center'>
-                  <p className="text-gray-100">1h:22m:18s</p>
+                  <p className="text-gray-100 text-lg">1h:22m:18s</p>
                   <p className="text-gray-400">Ideal Time</p>
                 </div>
               </div>
             </div>
             {/* summary section */}
             <p className="text-gray-100 text-center lg:text-start">Summary</p>
-            <div className="lg:h-80 w-full rounded-lg shadow-2xl my-3 flex flex-wrap md:flex-nowrap md:flex-row justify-center items-center lg:items-start">
-              <div className="w-64 md:w-auto lg:w-4/12 h-full bg-gray-900 p-4 rounded-lg rounded-r flex flex-col justify-evenly">
+            <div className="w-full rounded-lg shadow-2xl my-3 flex flex-wrap md:flex-nowrap md:flex-row justify-center items-center lg:items-start">
+              <div className="md:w-auto h-full bg-gray-900 p-4 rounded-lg rounded-r flex flex-col justify-evenly">
                 <p className="text-gray-400">Vehicle Control</p>
-                <div className="flex justify-between items-center lg:items-start overflow-x-scroll scrollbar-hide my-2 lg:my-0">
+                <div className="flex justify-between items-center lg:items-start h-full scrollbar-hide my-2">
                   {/* drive duration */}
-                <div className="h-24 lg:h-28 w-[45%] shadow-xl rounded-md bg-gray-800 p-2 cursor-pointer">
+                <div className="h-36 shadow-xl rounded-md bg-gray-800 p-2 cursor-pointer">
                     <div className="bg-gray-900 p-2 rounded-md w-fit">
-                      <BsStopwatch className="text-gray-100 text-xl" />
+                      <BsStopwatch className="text-gray-100 text-4xl" />
                     </div>
-                    <p className="text-[0.6rem] w-20 text-gray-200 font-semibold my-1">Drive Duration</p>
+                    <p className="text-sm w-24 text-gray-200 font-semibold my-3">Drive Duration</p>
                     <Switch defaultChecked className='-ml-2 -mt-1' />
                   </div>
                   {/* alert sync */}
-                  <div className="h-24 lg:h-28 w-[45%] shadow-xl rounded-md bg-gray-800 p-2 cursor-pointer">
+                  <div className="h-36 ml-2 shadow-xl rounded-md bg-gray-800 p-2 cursor-pointer">
                     <div className="bg-gray-900 p-2 rounded-md w-fit">
-                      <AiOutlineWarning className="text-gray-100 text-xl" />
+                      <AiOutlineWarning className="text-gray-100 text-4xl" />
                     </div>
-                    <p className="text-[0.6rem] w-20 text-gray-200 font-semibold my-1">Alert Sync</p>
+                    <p className="text-sm w-24 text-gray-200 font-semibold my-3">Alert Sync</p>
                     <Switch className='-ml-2 -mt-1' />
                   </div>
                 </div>
-                <div className="shadow-xl lg:h-16 w-full rounded-md bg-gray-800 p-1 flex items-center justify-evenly">
-                  <div className="w-[45%] h-full flex justify-center items-center">
+                <div className="shadow-xl w-full rounded-md bg-gray-800 p-1 flex items-center justify-evenly">
+                  <div className="h-full flex justify-center items-center">
                     <CircularProgressbar
-                      className="w-16 lg:w-14"
+                      className="w-16"
                       styles={buildStyles( {
                         rotation: 0.5,
                         strokeLinecap: 'butt',
@@ -80,15 +80,15 @@ const FuelEfficiency = () => {
                         pathTransitionDuration: 0.5,
                         pathColor: `rgba(62, 152, 199, ${50 / 100})`,
                         textColor: 'white',
-                        trailColor: 'white',
-                        backgroundColor: 'white',
+                        trailColor: '#d6d6d6',
+                        backgroundColor: '#3e98c7',
                       } )}
                       value={50} text={`${50}%`} />
                   </div>
-                  <p className="text-center text-xs -ml-20 mt-14 lg:mt-10 bg-gray-800 text-gray-200">Fuel Economy</p>
-                  <div className="w-[45%] h-full flex justify-center items-center">
+                  <p className="text-center text-sm -ml-20 mt-16 lg:mt-12 bg-gray-800 text-gray-200">Fuel Economy</p>
+                  <div className="h-full flex justify-center items-center">
                     <CircularProgressbar
-                      className="w-16 lg:w-14"
+                      className="w-16"
                       styles={buildStyles( {
                         rotation: 0.25,
                         strokeLinecap: 'butt',
@@ -101,19 +101,19 @@ const FuelEfficiency = () => {
                       } )}
                       value={88} text={`${88}%`} />
                   </div>
-                  <p className="text-center text-xs -ml-20 mt-14 lg:mt-10 bg-gray-800 text-gray-200">Fuel Efficiency</p>
+                  <p className="text-center text-sm -ml-20 mt-16 lg:mt-12 bg-gray-800 text-gray-200">Fuel Efficiency</p>
                 </div>
-                <div className="shadow-xl h-12 w-full rounded-md bg-gray-800 p-2 flex space-x-2 my-2 lg:my-0">
+                <div className="shadow-xl mt-2 w-full rounded-md bg-gray-800 p-2 flex space-x-2 my-2">
                   <div className="p-2 rounded-md bg-gray-900 w-fit">
-                    <BsSpeedometer2 className="text-2xl text-gray-200 -mt-1" />
+                    <BsSpeedometer2 className="text-4xl text-gray-200 -mt-1" />
                   </div>
-                  <div className="text-xs">
+                  <div className="text-sm">
                     <p className="text-gray-200 font-semibold">50 Km/Hr</p>
                     <p className="text-gray-400">Average Speed</p>
                   </div>
                 </div>
               </div>
-              <div className='bg-gray-900 h-[19.5rem] md:w-96 lg:h-full lg:w-full md:rounded-tr-lg md:rounded-br-lg w-64 rounded-md md:rounded-none my-6 md:my-0'>
+              <div className='bg-white h-96 md:w-96 lg:h-[23rem] lg:w-full md:rounded-tr-lg md:rounded-br-lg w-96 rounded-md md:rounded-none my-6 md:my-0 overflow-hidden'>
               </div>
             </div>
             </div>
