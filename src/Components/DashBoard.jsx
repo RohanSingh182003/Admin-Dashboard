debeb5881c1bd428e8019ca2523afa3f38378880
Rohan Singh <rs7289579@gmail.com>
2022-09-19 08:25:46 +0530

    createing layout responsive

--- src/Components/DashBoard.jsx
+++ src/Components/DashBoard.jsx
@@ -11,14 +11,14 @@ import Sos from "./MissionCenter/Sos";
 
 const DashBoard = () => {
   return (
-    <div className='h-[88vh] w-full lg:py-6 xl:py-8 flex items-center justify-evenly flex-col'>
-      <div className="flex flex-wrap justify-evenly items-center w-full mb-4 xl:px-6">
-        <EngineLoad/>
+    <div className='h-full md:h-[88vh] w-full lg:py-6 xl:py-8 flex items-center justify-evenly flex-col overflow-hidden'>
+      <div className="flex flex-wrap justify-evenly items-center w-full my-4 xl:px-6">
+        <EngineLoad props={{load:70}}/>
         <EngineOnOff props={{state:'on'}}/>
         <TripDuration/>
       </div>
-      <div className="w-full md:w-10/12 h-[75%] bg-gray-900 rounded-md flex">
-        <div className="w-4/12 h-full bg-gray-900">
+      <div className="w-full flex-col md:flex-row md:w-10/12 md:h-[75%] bg-gray-900 rounded-md flex md:pr-2">
+        <div className="w-full md:w-5/12 xl:w-4/12 md:h-full bg-gray-900 flex flex-col justify-evenly">
           <p className="text-center text-xl my-2 text-gray-200">Mission Center</p>
           <div className="flex justify-evenly">
             {/* immobilization and sos */}
@@ -35,7 +35,7 @@ const DashBoard = () => {
           </div>
         </div>
         {/* map section */}
-        <div className="w-8/12 h-full bg-gray-900 relative text-gray-200">
+        <div className="w-full md:h-full my-2 md:my-0 h-96 md:w-7/12 xl:w-8/12 bg-gray-800 md:bg-gray-900 text-gray-200 md:rounded-md">
           <div className="h-[10%] w-full flex justify-evenly">
             <div className="w-[50%] border-r border-gray-500 grid place-items-center">
               <p><span className="font-bold">Speed: </span>NA Km/h</p>
@@ -44,7 +44,7 @@ const DashBoard = () => {
             <p><span className="font-bold">RPM: </span>NA</p>
             </div>
           </div>
-          <div className="h-[90%] bg-blue-600"></div>
+          <div className="h-[88%] bg-blue-600 w-full md:rounded-md"></div>
         </div>
       </div>
     </div>
--- src/Components/TopCards/EngineLoad.jsx
+++ src/Components/TopCards/EngineLoad.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { CircularProgressbar , buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
-const EngineLoad = () => {
+const EngineLoad = ({props}) => {
+  const percentage = props.load;
   return (
     <div className='bg-gray-800 py-2 px-4 flex justify-between items-center w-full md:w-64 rounded-md'>
         <p className='text-blue-200 font-bold'>Engine Load</p>
-        <div className='h-12 text-center'>PG Bar</div>
+        <div className='h-12 w-12 text-center'>
+        <CircularProgressbar
+  value={percentage}
+  text={`${percentage}%`}
+  styles={buildStyles({
+    rotation: 0.25,
+    strokeLinecap: 'butt',
+    textSize: '1.5rem',
+    pathTransitionDuration: 0.5,
+    pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
+    // textColor: 'green',
+    fontWeight:'10px',
+    trailColor: '#d6d6d6',
+    backgroundColor: '#3e98c7',
+  })}
+/>
+        </div>
     </div>
   )
 }
